20358b01bf4a24f2dc2ef3464a0c11356f849611
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-19 16:31:32 +0200

    makes it a stateful widget with a state class

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,10 +1,24 @@
+import 'package:animationz/src/widgets/cat.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+import '../widgets/cat.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
   HomeState createState() => HomeState();
 }
 
 class HomeState extends State<Home> {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Aminations')),
+      body: buildAnimation(),
+    );
+  }
+
+  Widget buildAnimation() {
+    return const Cat();
+  }
 }
